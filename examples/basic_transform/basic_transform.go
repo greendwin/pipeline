f8fdd15dd7367c273a9ee74aec44d0d7d9a23be8
Evgeniy A. Cymbalyuk <cimbaluk@gmail.com>
2025-04-27 23:07:39 +0300

    add `basic_transform` example

---
+++ examples/basic_transform/basic_transform.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	pl "github.com/greendwin/pipeline"
+)
+
+func sequence(pp *pl.Pipeline, count int) <-chan int {
+	return pl.Generate(pp, func(wr pl.Writer[int]) {
+		for k := range count {
+			if !wr.Write(k) {
+				return
+			}
+		}
+	})
+}
+
+type result struct {
+	x, y int
+}
+
+func square(pp *pl.Pipeline, threads int, nums <-chan int) <-chan result {
+	return pl.Transform(pp, threads, nums, func(x int) result {
+		log.Printf("square(%d): started", x)
+		time.Sleep(500 * time.Millisecond)
+		log.Printf("square(%d): finished", x)
+		return result{x, x * x}
+	})
+}
+
+func printNums(pp *pl.Pipeline, in <-chan result) pl.Signal {
+	return pl.Process(pp, 1, in, func(r result) {
+		log.Printf("process: %d -> %d", r.x, r.y)
+	})
+}
+
+func main() {
+	log.SetFlags(log.Ltime)
+
+	pp := pl.NewPipeline()
+
+	nums := sequence(pp, 20)
+	sq := square(pp, 4, nums)
+	finished := printNums(pp, sq)
+
+	if !finished.WaitFor(2 * time.Second) {
+		log.Println("timeout!")
+	}
+
+	log.Println("shutting down...")
+	pp.Shutdown()
+	log.Println("done.")
+}
---
+++ examples/basic_transform/go.mod
@@ -0,0 +1,7 @@
+module github.com/greendwin/pipeline/examples/basic_transform
+
+go 1.24.2
+
+replace github.com/greendwin/pipeline => ../..
+
+require github.com/greendwin/pipeline v0.0.0-00010101000000-000000000000
