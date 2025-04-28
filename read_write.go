a4409dd362e2d36678e6a951c6ddcd0e1d8c2e55
Evgeniy A. Cymbalyuk <cimbaluk@gmail.com>
2025-04-28 19:58:33 +0300

    WIP: add `ReadErr` function

--- read_write.go
+++ read_write.go
@@ -1,5 +1,7 @@
 package pipeline
 
+import "errors"
+
 func Write[T any](pp *Pipeline, out chan<- T, val T) bool {
 	select {
 	case out <- val:
@@ -18,3 +20,26 @@ func Read[T any](pp *Pipeline, in <-chan T) (val T, ok bool) {
 		return
 	}
 }
+
+var ErrChannelClosed = errors.New("channel closed")
+
+// read either channel value or an error from any triggered channel
+func ReadErr[T any](pp *Pipeline, in <-chan T, errs ...<-chan error) (T, error) {
+	select {
+	case val, ok := <-in:
+		if !ok {
+			return val, ErrChannelClosed
+		}
+		return val, nil
+
+	case err, ok := <-First(pp, errs...):
+		var empty T
+		if ok {
+			return empty, err
+		}
+		return empty, ErrChannelClosed
+	}
+}
+
+// TODO: rewrite `ReadErr` without `First`
+// TODO: add tests!
