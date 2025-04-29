9b11943445490080a7f74eff83ae363ebe717259
Evgeniy A. Cymbalyuk <cimbaluk@gmail.com>
2025-04-29 15:41:55 +0300

    WIP: add tests for `ReadErr`

--- read_write.go
+++ read_write.go
@@ -23,23 +23,36 @@ func Read[T any](pp *Pipeline, in <-chan T) (val T, ok bool) {
 
 var ErrChannelClosed = errors.New("channel closed")
 
-// read either channel value or an error from any triggered channel
-func ReadErr[T any](pp *Pipeline, in <-chan T, errs ...<-chan error) (T, error) {
+// read either a channel value or an error from the first triggered channel
+//
+// returns `ErrChannelClosed` if value channel was closed
+// returns `ErrCancelled` if pipeline in shutting down
+// fallback to `Read` if all `errs` are closed (return `ErrChannelClosed` if !ok)
+func ReadErr[T any](pp *Pipeline, in <-chan T, errs ...<-chan error) (val T, err error) {
+	var ok bool
 	select {
-	case val, ok := <-in:
-		if !ok {
-			return val, ErrChannelClosed
-		}
-		return val, nil
+	case <-pp.done.Chan():
+		err = ErrChannelClosed
+		return
 
-	case err, ok := <-First(pp, errs...):
-		var empty T
-		if ok {
-			return empty, err
+	case val, ok = <-in:
+		if !ok {
+			err = ErrChannelClosed
 		}
-		return empty, ErrChannelClosed
+		return
+
+	case err, ok = <-First(pp, errs...):
+		if ok {
+			return
+		}
+
+		// all error channels were closed, fall back to `Read`
+		val, ok = Read(pp, in)
+		if !ok {
+			err = ErrChannelClosed
+		}
+		return
 	}
 }
 
 // TODO: rewrite `ReadErr` without `First`
-// TODO: add tests!
--- read_write_test.go
+++ read_write_test.go
@@ -1,6 +1,7 @@
 package pipeline_test
 
 import (
+	"fmt"
 	"testing"
 
 	pl "github.com/greendwin/pipeline"
@@ -70,5 +71,146 @@ func TestReadNeverStuck(t *testing.T) {
 	})
 
 	checkShutdown(t, pp)
+	checkSignaled(t, finished)
+}
+
+func TestReadErr(t *testing.T) {
+	pp := pl.NewPipeline()
+	defer checkShutdown(t, pp)
+
+	res := make(chan int)
+
+	err1 := make(chan error)
+	err2 := make(chan error)
+
+	finished := pl.Run(pp, func() {
+		v, err := pl.ReadErr(pp, res, err1, err2)
+		assert.Nil(t, err)
+		assert.Equal(t, v, 42)
+	})
+
+	checkPending(t, finished)
+	res <- 42
+	checkSignaled(t, finished)
+}
+
+func TestReadErrFinishWithError(t *testing.T) {
+	for index := range 10 {
+		t.Run(fmt.Sprintf("fail on errs[%d]", index), func(t *testing.T) {
+			pp := pl.NewPipeline()
+			defer checkShutdown(t, pp)
+
+			res := make(chan int)
+
+			var willFail chan error
+			errs := make([]<-chan error, 10)
+			for k := range errs {
+				cherr := make(chan error)
+				if k == index {
+					willFail = cherr
+				}
+				errs[k] = cherr
+			}
+
+			finished := pl.Run(pp, func() {
+				_, err := pl.ReadErr(pp, res, errs...)
+				assert.Equal(t, err, errTest)
+			})
+
+			checkPending(t, finished)
+			willFail <- errTest
+			checkSignaled(t, finished)
+		})
+	}
+}
+
+func TestReadErrReportChannelClosed(t *testing.T) {
+	pp := pl.NewPipeline()
+	defer checkShutdown(t, pp)
+
+	res := make(chan int)
+
+	err1 := make(chan error)
+	err2 := make(chan error)
+
+	finished := pl.Run(pp, func() {
+		_, err := pl.ReadErr(pp, res, err1, err2)
+		assert.Equal(t, err, pl.ErrChannelClosed)
+	})
+
+	checkPending(t, finished)
+	close(res)
+	checkSignaled(t, finished)
+}
+
+func TestReadErrReportCancelled(t *testing.T) {
+	pp := pl.NewPipeline()
+
+	res := make(chan int)
+	err1 := make(chan error)
+	err2 := make(chan error)
+
+	finished := pl.NewSignal()
+	go func() {
+		_, err := pl.ReadErr(pp, res, err1, err2)
+		assert.Equal(t, err, pl.ErrChannelClosed)
+		finished.Set()
+	}()
+
+	checkPending(t, finished)
+	checkShutdown(t, pp)
+
+	checkSignaled(t, finished)
+}
+
+func TestReadErrFallbackToRead(t *testing.T) {
+	pp := pl.NewPipeline()
+	defer checkShutdown(t, pp)
+
+	res := make(chan int)
+	err1 := make(chan error)
+	err2 := make(chan error)
+
+	finished := pl.NewSignal()
+	go func() {
+		val, err := pl.ReadErr(pp, res, err1, err2)
+		assert.Nil(t, err)
+		assert.Equal(t, val, 42)
+		finished.Set()
+	}()
+
+	checkPending(t, finished)
+
+	// close both error channels, it still must wait for value
+	close(err1)
+	close(err2)
+	checkPending(t, finished)
+
+	res <- 42
+	checkSignaled(t, finished)
+}
+
+func TestReadErrReportWhanAllClosed(t *testing.T) {
+	pp := pl.NewPipeline()
+	defer checkShutdown(t, pp)
+
+	res := make(chan int)
+	err1 := make(chan error)
+	err2 := make(chan error)
+
+	finished := pl.NewSignal()
+	go func() {
+		_, err := pl.ReadErr(pp, res, err1, err2)
+		assert.Equal(t, err, pl.ErrChannelClosed)
+		finished.Set()
+	}()
+
+	checkPending(t, finished)
+
+	close(err1)
+	close(err2)
+	checkPending(t, finished)
+
+	close(res)
 	checkSignaled(t, finished)
 }
